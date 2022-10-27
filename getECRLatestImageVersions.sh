32f2ca157fafc7d401179166354f8d67bf57a9c5
Simon Davies <sipdx@outlook.com>
2022-10-27 07:40:23 -0700

    Pass argument

--- getECRLatestImageVersions.sh
+++ getECRLatestImageVersions.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-region=eu-west-2
+
+if [ $# -eq 0 ] ; then
+    echo "No region specified"
+    exit 1
+fi
+
+region=$1
 
 for repo in \
         $(aws ecr describe-repositories --region $region --query "repositories[].[repositoryName]" --output text --no-cli-pager)
 do
-        echo "$repo : $(aws ecr describe-images --repository-name $repo --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text --region $region)" 
-        
+        echo "$repo : $(aws ecr describe-images --repository-name $repo --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text --region $region)"
 done
